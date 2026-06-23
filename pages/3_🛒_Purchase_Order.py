1f043381a62ded0b4ba0f3518f37dd21b09aff11
printar232-creator <printar232@gmail.com>
2026-06-23 13:27:01 +0700

    Update 3_🛒_Purchase_Order.py

--- pages/3_🛒_Purchase_Order.py
+++ pages/3_🛒_Purchase_Order.py
@@ -3,8 +3,6 @@ import pandas as pd
 
 st.title("🛒 Module: ข้อมูลใบสั่งซื้อค้างส่ง (Open Purchase Orders)")
 
-if 'df_po' in st.session_state:
-    df = st.session_state['df_po']
     # เพิ่ม sheet_name=1 เข้าไปเพื่อบังคับให้อ่าน Sheet ที่สอง
 df_po = pd.read_excel(uploaded_file, sheet_name=1)
     
